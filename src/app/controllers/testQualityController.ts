5224ae08a8e0826bd68e57e473217a9e0711e8ef
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-27 16:39:30 -0300

    feat: add instances in constructor in controller

--- src/app/controllers/testQualityController.ts
+++ src/app/controllers/testQualityController.ts
@@ -4,16 +4,27 @@ import { Request, Response } from 'express'
 import { PetriNetInitializer } from '../usecases/petriNetInitializer'
 import { TestQualityUseCase } from '../usecases/testQualityUseCase'
 
-interface ITestQualityController {
-  checkProductQuality(req: Request, res: Response): Promise<Response>
-}
+export class TestQualityController implements ts.ITestQualityController {
+  petriNetInitializer: ts.IPetriNetInitializer
+  productQualityPetri: ts.IProductQualityPetri
+  testQualityUseCase: ts.ITestQualityUseCase
+  productsService: ts.IProductServices
+  qualityServices: ts.IQualityServices
+  productsRepo: ts.IProductRepository
 
-const petriNetInitializer = new PetriNetInitializer()
-const testQualityUseCase = new TestQualityUseCase(
-  petriNetInitializer.getPetriNet(),
-)
-const productsRepo = new ProductRepository()
-const productsService = new ProductServices(productsRepo)
+  constructor() {
+    this.productQualityPetri = new ts.ProductQualityPetri()
+    this.petriNetInitializer = new ts.PetriNetInitializer(
+      this.productQualityPetri,
+    )
+    this.productsRepo = new ts.ProductRepository()
+    this.productsService = new ts.ProductServices(this.productsRepo)
+    this.qualityServices = new ts.QualityServices()
+    this.testQualityUseCase = new ts.TestQualityUseCase(
+      this.petriNetInitializer.getPetriNet(),
+      this.qualityServices,
+    )
+  }
 
 export class TestQualityController implements ITestQualityController {
   public async checkProductQuality(req: Request, res: Response) {

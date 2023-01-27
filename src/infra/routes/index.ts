ac58ac572155d65969c7b323ac9fd6f4d0a59863
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-27 16:45:40 -0300

    feat: add health route

--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -1,10 +1,13 @@
-import { TestQualityController } from '@/src/app/controllers/testQualityController'
 import express from 'express'
+import { TestQualityController } from '@/controllers'
+import { health } from './health.routes'
 
 const routes = express.Router()
 
 const testQualityController = new TestQualityController()
 
-routes.get('/start', testQualityController.checkProductQuality)
+routes
+  .get('/health', health)
+  .get('/start', testQualityController.checkProductQuality)
 
 export default routes
